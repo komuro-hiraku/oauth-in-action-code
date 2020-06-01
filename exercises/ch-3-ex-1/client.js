c28d4bc71acf1454b728f82bdbd6da90aeff6cc1
komuro.hiraku <komuro.hiraku@classmethod.jp>
2020-06-01 15:34:14 +0900

    Implements OAuth Client
    - feat: implements Authorization Code Grant Flow at Client

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,8 +27,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -48,7 +48,15 @@ app.get('/authorize', function(req, res){
 	/*
 	 * Send the user to the authorization server
 	 */
+	state = randomstring.generate();	// 3.2.3 Add state
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',	// 認証タイプはAuthorizationCodeGrant
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],	// redirectURLは先頭を使う
+		state: state		// 3.2.3 Add state
+	});
 	
+	res.redirect(authorizeUrl);	// 302 redirect指示
 });
 
 app.get('/callback', function(req, res){
@@ -56,7 +64,38 @@ app.get('/callback', function(req, res){
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
+
+	// Add Check State Value
+	if (req.query.state != state) {
+		res.render('error', {error: 'State value did not match'});
+		return;
+	}
+
+	var code = req.query.code;	// auth codeの取得
+
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+
+	// for authorize to authorizationServer
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, 
+			client.client_secret) // Authorizationヘッダを作成（Basic認証）
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+
+	var body = JSON.parse(tokRes.getBody());		// JSON Parse
+	access_token = body.access_token	// あとで使えるように変数に格納
 	
+	// 最終的にATとScopeを表示する。普通はこんなことしてはいけない（危険）
+	res.render('index', {access_token: body.access_token, scope: scope});
 });
 
 app.get('/fetch_resource', function(req, res) {
@@ -64,9 +103,31 @@ app.get('/fetch_resource', function(req, res) {
 	/*
 	 * Use the access token to call the resource server
 	 */
-	
+	// AccessTokenの存在チェック
+	if (!access_token) {
+		res.render('error', {error: 'Missing access token.'});
+		return;
+	}
+
+	// AccessTokenをヘッダに付与して保護してるリソースへアクセスする
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+	var resource = request('POST', protectedResource, { headers: headers});
+
+	// StatusCodeが200系ならOK
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+		res.render('data', {resource: body});
+		return;
+	} else {
+		// StatusCodeが200系以外はエラー
+		res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
+		return;
+	}
 });
 
+// クエリパラメータを付与したURLを作ってくれる関数
 var buildUrl = function(base, options, hash) {
 	var newUrl = url.parse(base, true);
 	delete newUrl.search;
