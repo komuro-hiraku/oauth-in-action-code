f21806819e69c64658594700d91454e58d3cd620
komuro.hiraku <komuro.hiraku@classmethod.jp>
2020-06-03 22:30:12 +0900

    - 4.2まで - AccessTokenの取得と評価

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -17,16 +17,48 @@ app.set('json spaces', 4);
 app.use('/', express.static('files/protectedResource'));
 app.use(cors());
 
+// 全ての処理に先んじて AccessToken の取得とチェックが必要
+// app.all("*", getAccessToken);
+
+// Bearerトークンの処理方法は3つ。Authorization ヘッダ、form_encoded な Body、クエリパラメータ
+// Default は一番推奨される Authorization ヘッダの方式を採用する
+
 var resource = {
 	"name": "Protected Resource",
 	"description": "This data has been protected by OAuth 2.0"
 };
 
 var getAccessToken = function(req, res, next) {
-	/*
-	 * Scan for an access token on the incoming request.
-	 */
-	
+	// Bearer トークンを取得する
+	var inToken = null;
+	var auth = req.headers['authorization'];	// ヘッダから Authorization を取り出す
+	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+
+		inToken = auth.slice('bearer '.length);	// bearer の後ろに一つ空白あり〼
+
+	} else if (req.body && req.body.access_token) {
+		// body に AccessToken が入力されている場合
+		inToken = req.body.access_token;
+	} else if (req.query && req.query.access_token) {
+		// クエリパラメータに AccessToken が入力されている場合
+		// ログに残ったり、リファラを通して不用意に漏れたりする可能性があるため、やむを得ない場合以外非推奨
+		inToken = req.query.access_token;
+	}
+
+	nosql.one(function(token) {
+		if (token.access_token == inToken) {
+			return token;
+		}
+	}, function(err, token) {
+		if (token) {
+			console.log("We found a matching token: %s", inToken);
+		} else {
+			console.log("No matching token was found.");
+		}
+		req.access_token = token;
+		next();
+		return;
+	});
 };
 
 app.options('/resource', cors());
@@ -35,12 +67,14 @@ app.options('/resource', cors());
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res){
+app.post("/resource", getAccessToken, cors(), function(req, res){
 
-	/*
-	 * Check to see if the access token was found or not
-	 */
-	
+	// getAccessToken を追加したことで事前に AccessToken のチェックが入る
+	if (req.access_token) {
+		res.json(resource);
+	} else {
+		res.status(401).end();
+	}
 });
 
 var server = app.listen(9002, 'localhost', function () {
